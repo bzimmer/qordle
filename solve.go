2c35729098646dd677046b112f7159b8138eaa0d
Brian Zimmer <bzimmer@ziclix.com>
2022-03-10 09:38:53 -0800

    added elapsed

--- solve.go
+++ solve.go
@@ -12,7 +12,7 @@ import (
 type FilterFunc func(string) bool
 
 func Solve(words Dictionary, fns ...FilterFunc) Dictionary {
-	var res []string
+	res := make([]string, 0)
 	for _, word := range words {
 		matches := true
 		for i := 0; matches && i < len(fns); i++ {
