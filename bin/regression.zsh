d22d7baa25607bf25cd62acd5ba2b37e72fce56e
Brian Zimmer <bzimmer@ziclix.com>
2023-04-17 20:51:29 +0200

    progress bar only if terminal

--- bin/regression.zsh
+++ bin/regression.zsh
@@ -9,21 +9,28 @@ strategies=(
     '-s el --start tares'
     '-s freq -s bigram'
     '-s freq -s el --start tares'
+    '-S -s freq -s el --start tares'
     '-s freq -s el -s bigram --start tares'
     '-s freq -s pos -s bigram -s el --start tares'
     '-s freq -s pos -s bigram'
     '-s freq -s pos'
+    '-S -s freq -s pos'
     '-s freq'
+    '-S -s freq'
     '-s pos'
+    '-S -s pos'
 )
 words=$(cat ${repo}/data/possible.txt | sort -R | tail -n ${N})
+
+progress=""
+if [[ -t 1 ]] ; then progress="-B" ; fi
 
 regression=${repo}/dist/regression.csv
 jq -rn '["secret", "strategy", "rounds", "success", "dictionary", "elapsed"] | @csv' > ${regression}
 for strategy in ${strategies}
 do
     print -P "%F{magenta}\nRunning {$strategy} strategy...%f\n" 1>&2
-    eval "${repo}/dist/qordle play -B -S ${strategy}" <<< ${words} |
+    eval "${repo}/dist/qordle play ${progress} ${strategy}" <<< ${words} |
     jq -r -s '
         map([.secret, .strategy, (.rounds|length), (.rounds|last|.success), (.rounds|last|.dictionary), .elapsed])
         | .[]
