3a6e0622f7dac661e21a512bd4009cf01c55ad9e
Brian Zimmer <bzimmer@ziclix.com>
2023-05-02 07:48:50 +0200

    improved tests

--- digits_test.go
+++ digits_test.go
@@ -1,10 +1,12 @@
 package qordle_test
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/urfave/cli/v2"
 	"golang.org/x/net/context"
 
 	"github.com/bzimmer/qordle"
@@ -39,23 +41,55 @@ func TestOerations(t *testing.T) {
 
 func TestDigits(t *testing.T) {
 	t.Parallel()
-	a := assert.New(t)
+	for _, tt := range []struct {
+		name string
+		f    func(*assert.Assertions, qordle.Digits)
+	}{
+		{
+			name: "success",
+			f: func(a *assert.Assertions, digits qordle.Digits) {
+				var candidates []qordle.Candidate
+				for c := range digits.Play(context.Background(), qordle.Board{1, 2, 6, 3}, 5) {
+					candidates = append(candidates, c)
+				}
+				a.Len(candidates, 13)
+			},
+		},
+		{
+			name: "exit with deadline",
+			f: func(a *assert.Assertions, digits qordle.Digits) {
+				ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(-time.Hour))
+				defer cancel()
 
-	var digits qordle.Digits
-	var candidates []qordle.Candidate
-	for c := range digits.Play(context.Background(), qordle.Board{1, 2, 6, 3}, 5) {
-		candidates = append(candidates, c)
+				var candidates []qordle.Candidate
+				candidates = []qordle.Candidate{}
+				for c := range digits.Play(ctx, qordle.Board{1, 2, 6, 3}, 5) {
+					candidates = append(candidates, c)
+				}
+				a.Len(candidates, 0)
+			},
+		},
+		{
+			name: "exit with cancel",
+			f: func(a *assert.Assertions, digits qordle.Digits) {
+				ctx, cancel := context.WithCancel(context.Background())
+				c := digits.Play(ctx, qordle.Board{2, 3, 8}, 5)
+				_, ok := <-c
+				a.True(ok)
+				cancel()
+				_, ok = <-c
+				a.False(ok)
+				a.Error(ctx.Err())
+			},
+		},
+	} {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var digits qordle.Digits
+			tt.f(assert.New(t), digits)
+		})
 	}
-	a.Len(candidates, 13)
-
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(-time.Hour))
-	cancel()
-
-	candidates = []qordle.Candidate{}
-	for c := range digits.Play(ctx, qordle.Board{1, 2, 6, 3}, 5) {
-		candidates = append(candidates, c)
-	}
-	a.LessOrEqual(len(candidates), 0)
 }
 
 func TestDigitsCommand(t *testing.T) {
@@ -68,6 +102,15 @@ func TestDigitsCommand(t *testing.T) {
 			name: "invalid number",
 			args: []string{"digits", "-t", "413", "20", "23", "aa"},
 			err:  `failed to convert 'aa'`,
+		},
+		{
+			name: "encoding error",
+			args: []string{"digits", "-t", "5", "1", "2", "6", "3"},
+			before: func(c *cli.Context) error {
+				qordle.Runtime(c).Encoder = json.NewEncoder(new(errWriter))
+				return nil
+			},
+			err: ErrEncoding.Error(),
 		},
 	} {
 		tt := tt
