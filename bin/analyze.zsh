44b83a10490857ea85ed869a3ff3cf9e071048ca
Brian Zimmer <bzimmer@ziclix.com>
2025-02-15 19:49:48 +0100

    replace csvq with duckdb

--- bin/analyze.zsh
+++ bin/analyze.zsh
@@ -6,55 +6,52 @@ repo=$(git rev-parse --show-toplevel)
 regression="${repo}/dist/regression.csv"
 
 print -P "%F{magenta}\nFailed to find any solution!%f\n" 1>&2
-csvq -c '
+duckdb -s '
     select
         secret, strategy, success
     from
-        stdin
+        read_csv("/dev/stdin")
     where
         success is false
     order by
-        strategy, secret
+        strategy, secret;
 ' < ${regression}
 
 print -P "%F{magenta}\nFailed to find the solution in six rounds!%f\n" 1>&2
-csvq -c '
+duckdb -s '
     select
         secret, strategy, rounds, success, elapsed
     from
-        stdin
+        read_csv("/dev/stdin")
     where
         rounds > 6
     order by
-        rounds, secret, strategy
+        rounds, secret, strategy;
 ' < ${regression}
 
 print -P "%F{magenta}\nHistograms%f\n" 1>&2
-csvq -c '
+duckdb -s '
     select
         strategy, rounds, count(rounds) as bins
     from
-        stdin
+        read_csv("/dev/stdin")
     group by
         strategy, rounds
     order by
-        len(strategy), strategy, rounds, bins
+        len(strategy), strategy, rounds, bins;
 ' < ${regression}
 
 print -P "%F{magenta}\nWinners%f\n" 1>&2
-csvq -c '
-    with
+duckdb -s '
+    create table
         totals
-    as (
-        select
-            strategy, count(*) as total
-        from
-            stdin
-        group by
-            strategy
-    )
+    as select
+        strategy, count(*) as total from read_csv("/dev/stdin") group by strategy;
+    create table
+        stdin
+    as select * from read_csv("/dev/stdin");
     select
-        stdin.strategy, count(stdin.rounds) as winners, totals.total, format("%0.1f", 100 * (float(winners) / totals.total)) as pct
+        stdin.strategy, count(stdin.rounds) as winners, totals.total, 100 * (winners / totals.total) as pct
     from
         stdin, totals
     where
@@ -63,5 +60,5 @@ csvq -c '
     group by
         stdin.strategy, totals.total
     order by
-        winners desc, stdin.strategy
+        winners desc, stdin.strategy;
 ' < ${regression}
